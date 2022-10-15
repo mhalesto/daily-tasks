a86e6fda69d425677d3428dcc1f5c8f0fc42bac8
mhalesto <mbanjwa.hg@gmail.com>
2022-10-15 10:50:17 +0200

    fix: seting up unit tests

---
+++ App.test.tsx
@@ -0,0 +1,8 @@
+import * as renderer from 'react-test-renderer';
+
+import App from './App';
+
+it('renders without crashing', () => {
+  const rendered = renderer.create(<App />).toJSON();
+  expect(rendered).toBeTruthy();
+});
